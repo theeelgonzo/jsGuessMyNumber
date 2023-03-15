5ef8cc76f7318a2b210b13b85d82d6c347b694a0
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-14 21:38:03 -0500

    Add event handler for game logic.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number! 😁';
 
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Pick a number next time, Yark-O!';
+  }
+});
