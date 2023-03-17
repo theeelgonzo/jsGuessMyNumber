add246653dad5a06aab23a21d46a6904d8893cd3
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-16 20:23:12 -0500

    Fix error where score does not reset.

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('.score').textContent = 20;
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
